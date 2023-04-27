54fea41360f3b39c9300c760f560843dd1b717e4
Matt <mhgarry92@gmail.com>
2023-04-26 22:17:40 -0400

    post route

--- controllers/api_routes.js
+++ controllers/api_routes.js
@@ -9,3 +9,11 @@ router.get('/notes', (req, res) => {
     .then((notes) => res.json(notes))
     .catch((err) => res.status(500).json(err));
 });
+
+// post request
+router.post('/notes', (req, res) => {
+  store
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+});
