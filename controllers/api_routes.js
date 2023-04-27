e24f5915f5d3ed8859ab926864b4da771bc981a7
Matt <mhgarry92@gmail.com>
2023-04-26 22:13:17 -0400

    tested get request, works

--- controllers/api_routes.js
+++ controllers/api_routes.js
@@ -3,6 +3,9 @@ const router = require('express').Router();
 const store = require('..//config/store');
 
 // get request
-router.get('/.notes', function (req , res) {
-	store.getNotes.then(note => res.json(note))
-}
+router.get('/notes', (req, res) => {
+  store
+    .getNotes()
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
