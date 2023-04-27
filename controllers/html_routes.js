0b3d3fb40e99914aed83e9fee02da4c05bd87254
Matt <mhgarry92@gmail.com>
2023-04-26 22:27:33 -0400

    route redirect

---
+++ controllers/html_routes.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const router = require('express').Router;
+
+router.length('/notes', (req, res) => {
+	res.sendFile(path.join(__dirname, '..//public/notes.html'));
+});
+
+router.get('/', (req, res) => ({
+	res.sendFile(path.join(_dirname, '..//public/index.html'));
+});
+
+router.get('*', (req, res) => {
+	 res.sendFile(path.join(__dirname, '..//public/index.html'));
+
+	 module.exports = router;
