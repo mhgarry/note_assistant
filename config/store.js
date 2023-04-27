36ff31ad8ccb2138216f5b2aa6ab45d65e9dccda
Matt <mhgarry92@gmail.com>
2023-04-26 20:07:19 -0400

    get and return notes from database

--- config/store.js
+++ config/store.js
@@ -6,14 +6,34 @@ const fs = require('fs/promises');
 // to generate a unique id for each note
 const { v4: uuidv4 } = require('uuid');
 
-//read and write notes
+// read and write notes
 const readFileAsync = util.promisfy(fs.readFile);
 const writeFileAsync = util.promisfy(fs.writeFile);
 
-class SaveNote {
-	write(note){
-		return writeFileAsync('.db/db.json' , JSON.stringify(note));
-	};
-	read(){
-    return readFileAsync('.db/db.json', 'utf8');
-  };
+class Store {
+  write(note) {
+    return writeFileAsync('./db/db.json', JSON.stringify(note));
+  }
+
+  read() {
+    return readFileAsync('./db/db.json');
+  }
+	//gets the notes from the database and returns them as an array
+  getNote() {
+		return this.read().then((notes => {
+			let convertedNotes;
+      try {
+				convertedNotes = [].concat(JSON.parse(notes));
+			} catch (err) {
+				convertedNotes = [];
+			}
+			return convertedNotes;
+    });
+	}
+
+	addNote(note) {
+		const { title, text } = note;
+		if (!title ||!text) {
+			throw new Error('Note must have a title and text inside');
+    }
+	}
