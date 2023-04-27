730bb808b4db9a9b5ac1d75854c0d2d1df1f6730
Matt <mhgarry92@gmail.com>
2023-04-26 22:07:13 -0400

    refactored to use promises instead of callbacks

--- config/store.js
+++ config/store.js
@@ -1,57 +1,53 @@
-// code to save, read, update, and delete notes from the database db.json file
-const util = require('util'); // brings in our utiiity functions specifically util.promisify
+// refactored again to use promises instead of callbacks
+const util = require('util');
 const fs = require('fs');
-const { v4: uuidv4 } = require('uuid'); // makes a unique id for each note
-// reads files from the database db.json file and writes them to the db.json file
+const { v4: uuidv4 } = require('uuid');
+
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
-// make a class called store to read and write note from the database db.json file
+
 class Store {
-  read() {
-    return readFileAsync('db/db.json', 'utf8');
+  constructor(filePath) {
+    this.filePath = filePath;
   }
 
-  // writes note to the database db.json file
-  write(note) {
-    return writeFileAsync('db/db.json', JSON.stringify(note));
+  async read() {
+    try {
+      const data = await readFileAsync(this.filePath, 'utf8');
+      return JSON.parse(data) || [];
+    } catch (error) {
+      return [];
+    }
   }
 
-  // get all notes from the database db.json file
-  getNotes() {
-    return this.read().then((notes) => {
-      let parsedNotes;
-      try {
-        parsedNotes = [].concat(JSON.parse(notes));
-      } catch (err) {
-        parsedNotes = [];
-      }
-
-      return parsedNotes;
-    });
+  async write(notes) {
+    await writeFileAsync(this.filePath, JSON.stringify(notes));
   }
 
-  // add note to the database db.json file
-  addNote(note) {
+  async getNotes() {
+    const notes = await this.read();
+    return notes;
+  }
+
+  async addNote (note) {
     const { title, text } = note;
 
     if (!title || !text) {
       throw new Error('Note title and text cannot be blank');
     }
 
+    const notes = await this.read();
     const newNote = { title, text, id: uuidv4() };
-
-    return this.getNotes()
-      .then((notes) => [...notes, newNote])
-      .then((updatedNotes) => this.write(updatedNotes))
-      .then(() => newNote);
+    notes.push(newNote);
+    await this.write(notes);
+    return newNote;
   }
 
-  // delete note from the database db.json file
-  removeNote(id) {
-    return this.getNotes()
-      .then((notes) => notes.filter((note) => note.id !== id))
-      .then((filteredNotes) => this.write(filteredNotes));
+  async removeNote (id) {
+    const notes = await this.read();
+    const filteredNotes = notes.filter((note) => note.id !== id);
+    await this.write(filteredNotes);
   }
 }
-// export the store class to be used to save, read, update, and delete notes fro the database db.json file
+
 module.exports = Store;
--- controllers/api_routes.js
+++ controllers/api_routes.js
@@ -1,1 +1,8 @@
 
+const router = require('express').Router();
+const store = require('..//config/store');
+
+// get request
+router.get('/.notes', function (req , res) {
+	store.getNotes.then(note => res.json(note))
+}
